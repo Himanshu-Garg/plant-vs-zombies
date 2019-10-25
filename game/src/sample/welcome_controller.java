4330e1c2f783e71c0fde83cccfebf760e7f8e8c9
himanshu <himanshu@himanshus-MacBook-Pro.local>
2019-10-25 19:38:26 +0530

    new game button working

--- game/src/sample/welcome_controller.java
+++ game/src/sample/welcome_controller.java
@@ -1,10 +1,12 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.event.*;
+import javafx.stage.Stage;
 
 
 public class welcome_controller {
@@ -28,8 +30,22 @@ public class welcome_controller {
 
     }
 
+    /**
+     * will change scene when new game button is being clicked...
+     * i.e when this function will run, scene will be changed.
+     * **/
     @FXML
-    void new_game_clicked(ActionEvent event) {
+    void new_game_clicked(ActionEvent event) throws Exception{
+        Parent lawn_parent = FXMLLoader.load(getClass().getResource("/fxml/lawn.fxml"));
+        Scene lawn_scene = new Scene(lawn_parent);
+
+        // following line is used to get the stage information...
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        // setting scene to window and displaying window...
+        window.setScene(lawn_scene);
+        window.show();
+
 
     }
 
