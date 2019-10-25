d0457569ac573147555dde133d28f7b70374d77e
himanshu <himanshu@himanshus-MacBook-Pro.local>
2019-10-25 18:05:55 +0530

    added button and exit working

--- game/src/sample/welcome_controller.java
+++ game/src/sample/welcome_controller.java
@@ -1,4 +1,36 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.event.*;
+
+
 public class welcome_controller {
+
+    @FXML
+    private Button new_game_button;
+
+    @FXML
+    private Button load_button;
+
+    @FXML
+    private Button exit_button;
+
+    @FXML
+    void exit_clicked(ActionEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    void load_game_clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void new_game_clicked(ActionEvent event) {
+
+    }
+
 }
