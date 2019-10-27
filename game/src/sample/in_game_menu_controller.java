63ab3d81bb24e47bcf11520e0ade76d0ac89efc9
himanshu <himanshu@himanshus-MacBook-Pro.local>
2019-10-27 14:48:53 +0530

    menu completed

--- game/src/sample/in_game_menu_controller.java
+++ game/src/sample/in_game_menu_controller.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.effect.BoxBlur;
+import javafx.scene.effect.Glow;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
@@ -11,6 +12,15 @@ public class in_game_menu_controller {
 
     @FXML
     private ImageView back;
+
+    @FXML
+    private ImageView restart;
+
+    @FXML
+    private ImageView save;
+
+    @FXML
+    private ImageView exit;
 
     private Stage lawn_window;
 
@@ -26,8 +36,92 @@ public class in_game_menu_controller {
         window.close();
     }
 
+    @FXML
+    void back_entered(MouseEvent event) {
+        glow_image(back);
+    }
+
+    @FXML
+    void back_exited(MouseEvent event) {
+        unglow_image(back);
+    }
+
+    @FXML
+    void exit_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void exit_entered(MouseEvent event) {
+        glow_image(exit);
+    }
+
+    @FXML
+    void exit_exited(MouseEvent event) {
+        unglow_image(exit);
+    }
+
+    @FXML
+    void restart_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void restart_entered(MouseEvent event) {
+        glow_image(restart);
+    }
+
+    @FXML
+    void restart_exited(MouseEvent event) {
+        unglow_image(restart);
+    }
+
+    @FXML
+    void save_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void save_entered(MouseEvent event) {
+        glow_image(save);
+    }
+
+    @FXML
+    void save_exited(MouseEvent event) {
+        unglow_image(save);
+    }
+
+    // self-defined functions
+
+    private void glow_image(ImageView i){
+        Glow glow = new Glow();
+        glow.setLevel(0.7);
+        i.setEffect(glow);
+    }
+
+    private void unglow_image(ImageView i){
+        Glow glow = new Glow();
+        glow.setLevel(0);
+        i.setEffect(glow);
+    }
+
     void setLawn_window(Stage l) {
         lawn_window = l;
     }
 
+    // self-defined functions ends here ------
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
--- game/src/sample/lawn_controller.java
+++ game/src/sample/lawn_controller.java
@@ -6,12 +6,14 @@ import javafx.fxml.LoadException;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.effect.BoxBlur;
+import javafx.scene.effect.Glow;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.scene.image.ImageView;
 
 import java.io.IOException;
 
@@ -19,6 +21,9 @@ public class lawn_controller {
 
     @FXML
     private AnchorPane root_AnchorPane;
+
+    @FXML
+    private ImageView menu;
 
     @FXML
     void menu_clicked(MouseEvent event) throws IOException {
@@ -60,8 +65,45 @@ public class lawn_controller {
         window.initStyle(StageStyle.TRANSPARENT);
         window.showAndWait();
 
-
-
     }
 
+    @FXML
+    void menu_entered(MouseEvent event) {
+        glow_image(menu);
+    }
+
+    @FXML
+    void menu_exited(MouseEvent event) {
+        unglow_image(menu);
+    }
+
+    // self-defined functions
+
+    private void glow_image(ImageView i){
+        Glow glow = new Glow();
+        glow.setLevel(0.7);
+        i.setEffect(glow);
+    }
+
+    private void unglow_image(ImageView i){
+        Glow glow = new Glow();
+        glow.setLevel(0);
+        i.setEffect(glow);
+    }
+
+    // self-defined functions ends here ------
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
